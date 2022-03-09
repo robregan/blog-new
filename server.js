d44ab515d315eeec490f2bfa65259ff1fc050969
rob <robert_regan86@yahoo.com>
2022-03-09 13:51:30 -0500

    working version, had to move repo

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+require("dotenv").config({ path: "./config/.env" });
+const PORT = 3000 || process.env.PORT
+
+mongoose.connect(process.env.DB_STRING, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(PORT, () => {console.log(`Server runnin like hell on port ${PORT}`)})
